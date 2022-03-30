873573679591b49c4523a50c58c9176123effe85
John Mitchell <john.mitchell@teamcolab.com>
2022-03-30 16:54:24 -0400

    stub out news and videos pages SMCO01-81

---
+++ wp-content/themes/hot97/page-videos.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ */
+
+namespace App;
+
+use App\Http\Controllers\Controller;
+use Rareloop\Lumberjack\Http\Responses\TimberResponse;
+use Rareloop\Lumberjack\QueryBuilder;
+use Timber\Timber;
+use App\PostTypes\Post;
+use App\PostTypes\Page;
+use Timber\Term;
+use App\ViewModels\CardViewModel;
+use App\ViewModels\HeroViewModel;
+
+class PageVideosController extends Controller
+{
+    public function handle()
+    {
+        QueryBuilder::macro('category', function (int $term_id) {
+            $this->params['tax_query'] = [
+                [
+                    'taxonomy' => 'category',
+                    'field' => 'term_id',
+                    'terms' => $term_id,
+                ]
+            ];
+
+            return $this;
+        });
+
+        $context = Timber::get_context();
+        $page = new Page();
+
+        $context['post'] = $page;
+        $context['title'] = $page->title;
+        $context['content'] = $page->content;
+
+        $videos_page_config = get_field('video_page_fields', 'options');
+
+        return new TimberResponse('templates/front-page.twig', $context);
+    }
+}

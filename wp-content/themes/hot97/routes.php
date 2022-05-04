a6362c2c84de6a9c01c32008b0c150f008a32960
Francis Yaconiello <francis@teamcolab.com>
2022-05-04 13:04:32 -0400

    SMCO01-93 testing an issue with route on template controller

--- wp-content/themes/hot97/routes.php
+++ wp-content/themes/hot97/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use Rareloop\Lumberjack\Facades\Router;
+use Rareloop\Lumberjack\Http\ServerRequest;
+
 // use Zend\Diactoros\Response\HtmlResponse;
 
 // Router::get('hello-world', function () {
@@ -8,10 +10,10 @@ use Rareloop\Lumberjack\Facades\Router;
 // });
 
 
-Router::get('home-load-more', function () {
+Router::get('home-load-more', function (ServerRequest $request) {
     // Manually include the file
     include __DIR__ . '/home.php';
 
     // Manually call the controller method that you want to use
-    (new \App\HomeController)->loadMore();
+    (new \App\HomeController)->loadMore($request);
 });

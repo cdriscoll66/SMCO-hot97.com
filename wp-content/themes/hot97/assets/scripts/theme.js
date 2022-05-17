54b2a43a5e5420ef7f03e6e94fcde1d9051ef00c
John Mitchell <john.mitchell@teamcolab.com>
2022-05-17 14:13:27 -0400

    style flickity prev-next buttons SMCO01-122

--- wp-content/themes/hot97/assets/scripts/theme.js
+++ wp-content/themes/hot97/assets/scripts/theme.js
@@ -16,5 +16,11 @@ document.addEventListener('DOMContentLoaded', function() {
     contain: true,
     pageDots: false,
     cellAlign: 'left',
+    arrowShape: {
+      x0: 10,
+      x1: 60, y1: 50,
+      x2: 65, y2: 45,
+      x3: 20
+    },
   });
 });

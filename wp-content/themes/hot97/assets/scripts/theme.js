95201b06f580c6a6c2d482af113e2cd3567095bf
John Mitchell <john.mitchell@teamcolab.com>
2022-05-17 11:42:29 -0400

    implement basic flickity carousel SMCO01-122

--- wp-content/themes/hot97/assets/scripts/theme.js
+++ wp-content/themes/hot97/assets/scripts/theme.js
@@ -1,12 +1,20 @@
+import Flickity from 'flickity';
 import FancyBox from './modules/FancyBox.js';
 import LoadMorescroll from './modules/LoadMoreScroll';
 import MobileMenuDrawer from './modules/MobileMenuDrawer';
 import OpenSearch from './modules/OpenSearch';
-
 
 document.addEventListener('DOMContentLoaded', function() {
   FancyBox();
   LoadMorescroll();
   MobileMenuDrawer();
   OpenSearch();
+
+  let elem = document.querySelector('.js-dj-carousel');
+  let flkty = new Flickity( elem, {
+    freeScroll: true,
+    contain: true,
+    pageDots: false,
+    cellAlign: 'left',
+  });
 });

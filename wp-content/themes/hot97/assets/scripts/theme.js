25baac38c1ddcea3cf20d12d0ffcac40b7889a71
John Mitchell <john.mitchell@teamcolab.com>
2022-05-18 12:24:26 -0400

    add carousel functionality to featured carousel sections SMCO01-121

--- wp-content/themes/hot97/assets/scripts/theme.js
+++ wp-content/themes/hot97/assets/scripts/theme.js
@@ -1,8 +1,11 @@
 import Flickity from 'flickity';
+import matchHeight from 'jquery-match-height';
 import FancyBox from './modules/FancyBox.js';
 import LoadMorescroll from './modules/LoadMoreScroll';
 import MobileMenuDrawer from './modules/MobileMenuDrawer';
 import OpenSearch from './modules/OpenSearch';
+
+let $ = window.jQuery;
 
 document.addEventListener('DOMContentLoaded', function() {
   FancyBox();
@@ -10,17 +13,40 @@ document.addEventListener('DOMContentLoaded', function() {
   MobileMenuDrawer();
   OpenSearch();
 
-  let elem = document.querySelector('.js-dj-carousel');
-  let flkty = new Flickity( elem, {
-    freeScroll: true,
-    contain: true,
-    pageDots: false,
-    cellAlign: 'left',
-    arrowShape: {
-      x0: 10,
-      x1: 60, y1: 50,
-      x2: 65, y2: 45,
-      x3: 20
-    },
-  });
+  let DJCarousels = document.querySelectorAll('.js-dj-carousel');
+  for ( let i=0, len = DJCarousels.length; i < len; i++ ) {
+    let carousel = DJCarousels[i];
+    new Flickity( carousel, {
+      freeScroll: true,
+      contain: true,
+      pageDots: false,
+      cellAlign: 'left',
+      arrowShape: {
+        x0: 10,
+        x1: 60, y1: 50,
+        x2: 65, y2: 45,
+        x3: 20
+      },
+    });
+  }
+
+  let featuredCarousels = document.querySelectorAll('.js-featured-carousel');
+  for ( let i=0, len = featuredCarousels.length; i < len; i++ ) {
+    let carousel = featuredCarousels[i];
+    new Flickity( carousel, {
+      freeScroll: true,
+      contain: true,
+      pageDots: false,
+      cellAlign: 'left',
+      arrowShape: {
+        x0: 10,
+        x1: 60, y1: 50,
+        x2: 65, y2: 45,
+        x3: 20
+      },
+    });
+  }
+
+  $('.js-featured-carousel-slide').matchHeight();
+
 });

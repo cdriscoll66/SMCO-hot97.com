3b16fff4cc5b16f74bd59bf640c2734d95fc7e76
John Mitchell <john.mitchell@teamcolab.com>
2022-05-31 13:22:51 -0400

    add more content and related posts to post controller SMCO01-148

--- wp-content/themes/hot97/single-post.php
+++ wp-content/themes/hot97/single-post.php
@@ -10,9 +10,42 @@ use App\Http\Controllers\Controller;
 use Rareloop\Lumberjack\Http\Responses\TimberResponse;
 use App\PostTypes\Post;
 use Timber\Timber;
+use Rareloop\Lumberjack\QueryBuilder;
+use App\ViewModels\CardViewModel;
 
 class SinglePostController extends Controller
 {
+    public function getRelatedPosts($tags, $exclude)
+    {
+        // Create macro for tags
+        QueryBuilder::macro('tags', function (object $tags) {
+            $slugs = [];
+
+            foreach ($tags as $tag) {
+                $slugs[] = $tag->slug;
+            }
+
+            $this->params['tax_query'] = [
+                'relation' => 'OR',
+                [
+                    'taxonomy' => 'post_tag',
+                    'field' => 'slug',
+                    'terms' => $slugs,
+                ]
+            ];
+
+            return $this;
+        });
+
+        $posts = Post::builder()
+            ->whereIdNotIn($exclude)
+            ->tags($tags)
+            ->orderBy('date', 'desc')
+            ->get();
+
+        return $posts;
+    }
+
     public function handle()
     {
         $context = Timber::get_context();
@@ -23,8 +56,9 @@ class SinglePostController extends Controller
         $context['content'] = $post->content;
         $context['main_class'] = 'o-main--single-post';
         $context['tags']= $post->getTags();
-        $context['more_posts'] = [];
-        $context['related_posts'] = [];
+        // more_content is the "more from hot97" in the post sidebar
+        $context['more_content'] = get_field('more_content', $post->id);
+        $context['related_posts'] = $this->getRelatedPosts($context['tags'], [$post->ID]);
 
         return new TimberResponse('templates/single-post.twig', $context);
     }

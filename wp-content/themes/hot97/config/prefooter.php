31e2b662e3e766a478cf5b9b170c0c30f5fb1e95
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-06-02 15:02:31 -0400

    prefooter fix, editor style add and styles for dj wip

--- wp-content/themes/hot97/config/prefooter.php
+++ wp-content/themes/hot97/config/prefooter.php
@@ -10,12 +10,16 @@ add_filter('timber/context', function ($context) {
 
     $obj = get_queried_object();
 
+    if($obj) {
+
     $show_prefooter = get_field('show_prefooter', $obj->taxonomy.'_'.$obj->term_id) ?: get_field('show_prefooter');
 
     if($show_prefooter) {
         $prefooter = get_field('prefooter', $obj->taxonomy.'_'.$obj->term_id) ?: get_field('prefooter');
         $context['prefooter'] = $prefooter['prefooter_image'] ? $prefooter : get_field('prefooter', 'options');
     }
-
+    }
     return $context;
+
+
 });

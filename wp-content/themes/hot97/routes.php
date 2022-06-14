88370543943383e1b48f6fd119f5463db630e0e0
Conor Driscoll <conor.driscoll@teamcolab.com>
2022-06-14 17:16:10 -0400

    built remaining route structure

--- wp-content/themes/hot97/routes.php
+++ wp-content/themes/hot97/routes.php
@@ -4,3 +4,6 @@ use Rareloop\Lumberjack\Facades\Router;
 
 Router::get('home-load-more', 'HomeLoadMoreController@loadMore');
 Router::get('single-post-load-more/{postid}', 'SinglePostLoadMoreController@loadMore');
+Router::get('content-category-load-more/{postcat}', 'ContentCategoryLoadMoreController@loadMore');
+Router::get('category-feed-load-more/{cat_id}', 'CategoryLoadMoreController@loadMore');
+Router::get('search-results-load-more/{term}', 'SearchResultsLoadMoreController@loadMore');
